9c2ab45e79f003c958ca10ddb4dce6c0c4e093ed
6mdx <6mdxxx@gmail.com>
2025-11-06 17:14:26 +0300

    fix grid

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -51,7 +51,7 @@ export default function Challenges() {
             </div>
 
             {/* Challenges Grid */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
                 {filteredChallenges.map((challenge) => (
                     <ChallengeCard key={challenge.slug} challenge={{sloved:true, ...challenge}} />
                 ))}
