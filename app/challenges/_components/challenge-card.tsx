695fcde2fb05a97bd2242ec835846497425fdb77
6mdx <6mdxxx@gmail.com>
2025-11-07 14:07:50 +0300

    fix small screen responsive

--- app/challenges/_components/challenge-card.tsx
+++ app/challenges/_components/challenge-card.tsx
@@ -16,15 +16,15 @@ export function ChallengeCard({ challenge }: { challenge: ChallengeContent }) {
     const DifficultyIcon = DIFFICULTY_TOKEN[challenge.difficulty].Icon;
     const { solved } = useScoreStore()
     return (
-        <Card className="flex flex-col hover:shadow-lg transition-shadow">
+        <Card className="flex flex-col justify-between hover:shadow-lg transition-shadow">
             <CardHeader className="">
                 <CardTitle className="text-xl">{challenge.title}</CardTitle>
                 <CardDescription className="line-clamp-4">
                     {challenge.description}
                 </CardDescription>
             </CardHeader>
-            <CardFooter className="justify-between">
-                <div className="flex items-start gap-2">
+            <CardFooter className="sm:justify-between flex-col gap-1 w-full sm:flex-row ">
+                <div className="flex items-start self-start sm:self-center gap-2">
                     <Badge variant={DIFFICULTY_TOKEN[challenge.difficulty].badge}>
                         {DIFFICULTY_TOKEN[challenge.difficulty].label}
                         <DifficultyIcon className="ml-1 h-4 w-4" />
@@ -33,7 +33,7 @@ export function ChallengeCard({ challenge }: { challenge: ChallengeContent }) {
                         SOLVED <Check />
                     </Badge>}
                 </div>
-                <Button asChild size="sm">
+                <Button asChild size="sm" className="self-end sm:self-center">
                     <Link href={`/challenges/${challenge.slug}`}>
                         Start Challenge
                         <ArrowRight />
